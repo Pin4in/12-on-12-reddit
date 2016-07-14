3f396b0c0763be7dfd3eeefd1e24b6b7f715ec3c
Pin4in <sergii.korkh@gmail.com>
2016-07-14 17:09:43 +0300

    improve comments pubsub

--- client/views/post-page/post_page.js
+++ client/views/post-page/post_page.js
@@ -4,8 +4,5 @@ Template.postPage.helpers({
   },
   comments: function() {
     return Comments.find({postId: this._id});
-  },
-  commentsCount: function() {
-    return Comments.find({postId: this._id}).count();
   }
 });
--- server/publish.js
+++ server/publish.js
@@ -9,6 +9,7 @@ Meteor.publish('posts', function() {
 // *************
 // Publish Comments
 // *************
-Meteor.publish('comments', function() {
-  return Comments.find();
+Meteor.publish('comments', function(postId) {
+  check(postId, String);
+  return Comments.find({postId: postId});
 });
